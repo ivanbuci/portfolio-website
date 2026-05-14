230e2ff14b5d1436c48a1d927cc81c875f8f93bf
ivanbuci <ivanmiracle21@gmail.com>
2026-05-14 14:14:29 +0700

    update light mode

---
+++ script.js
@@ -0,0 +1,27 @@
+const title = document.querySelector(".title");
+
+const button = document.querySelector(".change-btn");
+
+button.addEventListener("click", function () {
+
+    title.innerHTML = "Javascript Developer";
+
+});
+
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+
+        themeBtn.innerHTML = "Dark Mode";
+
+    } else {
+
+        themeBtn.innerHTML = "Light Mode";
+
+    }
+
+});
