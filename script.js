74e2c0450850cf18c417e1519ec8f5ae99bb89a8
ivanbuci <ivanmiracle21@gmail.com>
2026-05-14 16:01:52 +0700

    update burger menu

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ themeBtn.addEventListener("click", function () {
     }
 
 });
+
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", function() {
+
+    navLinks.classList.toggle("active");
+    
+});
